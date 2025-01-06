a64bfe31bf0ab1f4f14db2795849448afe6989af
Maitree Shukla <maitreeshukla11@gmail.com>
2025-01-06 16:02:52 -0600

    Initial Commit

---
+++ octet/octetApp.swift
@@ -0,0 +1,17 @@
+//
+//  octetApp.swift
+//  octet
+//
+//  Created by Maitree Shukla on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct octetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
